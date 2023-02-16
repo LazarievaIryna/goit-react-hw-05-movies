051a0064a06bf005b4c683398a8754cc53e206fa
Iryna Lazareva <il337733@gmail.com>
2023-02-16 21:24:39 +0200

    change

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -6,26 +6,34 @@ export const MovieDetails = () => {
   const { movieId } = useParams();
   console.log(movieId);
 
-  const [movie, setMovie] = useState(null);
+  const [movie, setMovie] = useState({});
 
   useEffect(() => {
-    async function fetchMovie(movieId) {
-      try {
-        const data = getMovieById(movieId);
-        setMovie(data);
-        console.log(data);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchMovie(movieId);
+    // async function fetchMovie(movieId) {
+    //   try {
+    //     const data = getMovieById(movieId);
+    //     setMovie(data);
+    //     console.log(data);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // }
+    // fetchMovie(movieId);
+    getMovieById(movieId)
+      .then(movie => setMovie(movie))
+      .catch(error => console.log('Error:', error));
   }, [movieId]);
 
-  console.log(movie);
-  // const { title } = movie;
+  console.log(typeof movie.release_date);
+  const { title, vote_average, release_date } = movie;
   return (
     <div>
-      {/* <h1>{movie.title}</h1> */}
+      <h1>
+        {title} <span>{release_date.slice(0, 4)}</span>{' '}
+      </h1>
+      <p>
+        User Score: <span>{Math.round(vote_average * 10)} &#37;</span>
+      </p>
       <Outlet />
     </div>
   );
