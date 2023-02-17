6732a524c96206596b6b4d7ddffddeb5906b186a
Iryna Lazareva <il337733@gmail.com>
2023-02-17 22:06:06 +0200

    Update MovieDetails.js

--- src/components/MovieDetails.js
+++ src/components/MovieDetails.js
@@ -3,33 +3,37 @@ import { Outlet, useParams } from 'react-router-dom';
 import { getMovieById } from 'services/Api';
 
 export const MovieDetails = () => {
-  const { movieId } = useParams();
-  console.log(movieId);
-
   const [movie, setMovie] = useState({});
+  const { movieId } = useParams();
+  // console.log(movieId);
 
   useEffect(() => {
-    // async function fetchMovie(movieId) {
-    //   try {
-    //     const data = getMovieById(movieId);
-    //     setMovie(data);
-    //     console.log(data);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // }
-    // fetchMovie(movieId);
-    getMovieById(movieId)
-      .then(movie => setMovie(movie))
-      .catch(error => console.log('Error:', error));
+    async function fetchMovie(movieId) {
+      try {
+        const response = await getMovieById(movieId);
+        setMovie(response);
+        // console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchMovie(movieId);
+    // getMovieById(movieId)
+    //   .then(movie => setMovie(movie))
+    //   .catch(error => console.log('Error:', error));
   }, [movieId]);
 
-  console.log(typeof movie.release_date);
+  console.log(movie.release_date);
   const { title, vote_average, release_date } = movie;
+  // const str = movie.release_date;
+  // const date = str.slice(0, 4);
+  // console.log(release_date);
+
   return (
     <div>
       <h1>
-        {title} <span>{release_date.slice(0, 4)}</span>{' '}
+        {title}
+        <span>{release_date}</span>
       </h1>
       <p>
         User Score: <span>{Math.round(vote_average * 10)} &#37;</span>
